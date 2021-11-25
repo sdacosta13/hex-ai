9faf2fee5417beab31259ad2d446fb36d35db93c
Sam da Costa <samuel.dacosta-2@student.manchester.ac.uk>
2021-11-25 17:03:18 +0000

    Added checks to stop patterns being checked on the border of the board

--- ICA/InferiorCell.cpp
+++ ICA/InferiorCell.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <cstring>
-#include <time.h>
+#include <chrono>
 #include <climits>
 #include <tuple>
 using namespace std;
-
+using namespace std::chrono;
 
 #define BOARD_WIDTH 5
 #define USE_COLOR true
@@ -104,7 +104,7 @@ class Board{
 
 
 
-    tuple<bool, int> CheckForDeadCell(int x, int y){
+    tuple<bool, int> CheckForDeadCell(int x, int y){ // need to add handling for x< 0 && y < 0 etc.
       int colourSel1 = 0;
       int colourSel2 = 1;
       for(int colourNum = 0; colourNum < 2; colourNum++){
@@ -113,27 +113,44 @@ class Board{
           bool patternMatched = true;
           for(int redCoord = 0; redCoord < PATTERN_MAX_COORDS; redCoord++){
             if(patterns[patternIndex][colourSel1][redCoord][0] != X){
-              Tile res = GetBoard(x + patterns[patternIndex][colourSel1][redCoord][0], y + patterns[patternIndex][colourSel1][redCoord][1]);
-              if(res != PLAYER_RED){
+              int xToCheck = x + patterns[patternIndex][colourSel1][redCoord][0];
+              int yToCheck = y + patterns[patternIndex][colourSel1][redCoord][1];
+              if(!(xToCheck < 0 || yToCheck < 0 || xToCheck >= BOARD_WIDTH || yToCheck >= BOARD_WIDTH)){
+                Tile res = GetBoard(xToCheck, yToCheck);
+                if(res != PLAYER_RED){
+                  patternMatched = false;
+                  break; // For Performance stop checking
+                }
+              } else {
+                // When Culling against the edge of the board, I assume the pattern is not detected.
+                // Therefore it is possible that this function will return false for a cell which is not Dead
+                // I decided this was preferable to reporting true when the pixel was not dead
                 patternMatched = false;
+                break;
               }
             }
           }
-
-          for(int blueCoord = 0; blueCoord < PATTERN_MAX_COORDS; blueCoord++){
-            if(patterns[patternIndex][colourSel2][blueCoord][0] != X){
-              Tile res = GetBoard(x + patterns[patternIndex][colourSel2][blueCoord][0], y + patterns[patternIndex][colourSel2][blueCoord][1]);
-              if(res != PLAYER_BLUE){
-                patternMatched = false;
+          if(patternMatched){ // If devation from the pattern has been seen dont bother continuing checks
+            for(int blueCoord = 0; blueCoord < PATTERN_MAX_COORDS; blueCoord++){
+              if(patterns[patternIndex][colourSel2][blueCoord][0] != X){
+                int xToCheck = x + patterns[patternIndex][colourSel2][blueCoord][0];
+                int yToCheck = y + patterns[patternIndex][colourSel2][blueCoord][1];
+                if(!(xToCheck < 0 || yToCheck < 0 || xToCheck >= BOARD_WIDTH || yToCheck >= BOARD_WIDTH)){
+                  Tile res = GetBoard(xToCheck, yToCheck);
+                  if(res != PLAYER_BLUE){
+                    patternMatched = false;
+                    break;
+                  }
+                } else {
+                  patternMatched = false;
+                  break;
+                }
               }
             }
           }
           if(patternMatched){
             return make_tuple(true, patternIndex);
           }
-
-
-
 
         }
         colourSel1 = 1;
@@ -165,14 +182,14 @@ class Board{
 
 
 int main() {
-  Board b(false);
+  Board b(true);
   int x = 2;
   int y = 2;
   b.SetBoard(x-1, y, Board::PLAYER_BLUE);
   b.SetBoard(x,   y-1,Board::PLAYER_BLUE);
   b.SetBoard(x+1, y-1, Board::PLAYER_BLUE);
   b.SetBoard(x+1, y, Board::PLAYER_BLUE);
-  clock_t before = clock();
+  auto t1 = high_resolution_clock::now();
   for(int y = 0; y < BOARD_WIDTH; y++){
    for(int x = 0; x < BOARD_WIDTH; x++){
      tuple<bool, int> result = b.CheckForDeadCell(x,y);
@@ -181,9 +198,9 @@ int main() {
      }
    }
   }
-  clock_t diff = clock() - before;
-  int msec = diff * 1000/ CLOCKS_PER_SEC;
+  auto t2 = high_resolution_clock::now();
+  duration<double, std::milli> ms_double = t2 - t1;
   b.PrintBoard();
-  cout << "Time taken: " << msec << endl;
+  cout << "Time taken: " << ms_double.count() << "ms" <<endl;
   return 0;
 };
